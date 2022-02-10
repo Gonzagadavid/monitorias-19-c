5899de27f15fb03bff61d96dddbd8ace647d3ceb
Gonzagadavid <gonzagadaviddev@gmail.com>
2022-02-10 12:53:02 -0300

    adicionado introducao de class

--- class/class.js
+++ class/class.js
@@ -1,19 +1,97 @@
+// class Animal {
+//   constructor(especie, nome) {
+//     this.especie = especie
+//     this.nome = nome
+//   }
+
+//   comer() {
+//     return `${this.nome} está comendo!`
+//   }
+// }
+
+// const rex = new Animal('cachorro', 'rex')
+
+// const xena = new Animal('gato', 'xena')
+
+// console.log(rex.comer())
+
+// console.log(xena.comer())
+
+
+///////////////////////////////////////////////////////////////
+
 class Animal {
-  constructor(nome, especie) {
-    this.nome = nome
+  constructor(especie, nome) {
     this.especie = especie
+    this.nome = nome
+    this.locacao = 'America do Sul'
+  }
+
+  info() {
+    return `${this.nome} é da especie ${this.especie}`
   }
 
   comer() {
-    console.log(`${this.nome} esta comendo`)
+    return `${this.nome} está comendo!`
   }
 }
 
-const cachorro = new Animal('totó', 'cachorro')
+class Cachorro extends Animal {
+  constructor(nome, raca){
+    super('cachorro', nome)
+    this.raca = raca
+  }
 
-const gato = new Animal('xena', 'gato')
-console.log(cachorro)
-console.log(gato)
+  getRaca() {
+    return `${this.nome} possui a raça ${this.raca}`
+  }
+  
+  latir() {
+    return `${this.nome} está latindo`
+  }
+}
 
-// cachorro.comer()
-// gato.comer()
+class PastorAlemao extends Cachorro {
+   constructor(nome) {
+     super(nome, 'Pastor alemão')
+   }
+
+   getSpecie() {
+     return this.info()
+   }
+  
+}
+
+class Dono extends cachorro {
+  
+}
+
+class Gato extends Animal {
+  constructor(nome) {
+    super('gato', nome)
+  }
+
+  miar() {
+    return `${this.nome} está miando`
+  }
+
+  briga() {
+    return `${this.name} está brigando com ${new Cachorro('Rex')}`
+  }
+}
+
+const rex = new Cachorro('rex', 'vira-lata')
+
+const thor = new Cachorro('Thor', 'pit-bull')
+
+const xena = new Gato('xena')
+
+// console.log(rex.latir())
+
+// console.log(xena.miar())
+
+// console.log(thor.latir())
+
+const raika = new PastorAlemao('Raika')
+
+console.log(Cachorro.getRaca())
